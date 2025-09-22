e0272485a4a6c1d908f43e0ca949e7fcb89d13be
CHIRLA SATYA DELEEP REDDY <satyadeleepreddychirla@gmail.com>
2025-09-22 12:31:18 +0530

    QuizBuilderBackendApplicationTests.java

--- src/test/java/com/example/demo/QuizBuilderBackendApplicationTests.java
+++ src/test/java/com/example/demo/QuizBuilderBackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootTest
-class QuizBuilderBackendApplicationTests {
+@SpringBootApplication
+public class QuizBuilderBackendApplication extends SpringBootServletInitializer {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizBuilderBackendApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QuizBuilderBackendApplication.class);
+    }
 }
